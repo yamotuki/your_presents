28d40f34fa0f51f80d535e2ca4b3105a49e0110e
Tomoya_Suzuki <mamemochi3ko@gmail.com>
2017-12-31 18:28:50 +0900

    投稿ページとその内容に合わせてdb migrateファイル書き換え

--- db/migrate/20171228135847_create_users.rb
+++ db/migrate/20171228135847_create_users.rb
@@ -1,9 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-      t.string :name
-      t.integer :age
-      t.string :sex
+      t.string :twitter_id
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,22 +13,15 @@
 ActiveRecord::Schema.define(version: 20171228141329) do
 
   create_table "posts", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "from"
-    t.string "to"
     t.string "situation"
-    t.string "item"
-    t.string "why"
-    t.string "reaction"
-    t.integer "success"
+    t.string "goods"
+    t.string "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.string "sex"
+    t.string "twitter_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
