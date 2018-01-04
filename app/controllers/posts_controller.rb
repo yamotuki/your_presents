3c968942b6e0cdb911d62dc62af9d9b676429972
Tomoya_Suzuki <mamemochi3ko@gmail.com>
2018-01-04 23:38:10 +0900

    最低限の投稿と表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,9 @@ class PostsController < ApplicationController
   end
   def new
   end
+  def create
+    @post = Post.new(situation: params[:situation], goods: params[:goods], comment: params[:comment])
+    @post.save
+    redirect_to("/home/top")
+  end
 end
