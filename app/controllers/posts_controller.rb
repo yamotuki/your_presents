a1dfa8776e67174c0fe3eaf7c9c578c473f53c76
Tomoya_Suzuki <mamemochi3ko@gmail.com>
2018-01-06 22:32:43 +0900

    画像を投稿して雑ながら表示できるように

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,19 @@ class PostsController < ApplicationController
   def new
   end
   def create
-    @post = Post.new(situation: params[:situation], goods: params[:goods], comment: params[:comment])
+    @post = Post.new(
+      situation: params[:situation],
+      goods: params[:goods],
+      comment: params[:comment]
+    )
+    # 画像の扱いは carrierWaveと言うGemが楽らしい。とりあえずベーシックな方法でやる。
+    if params[:image]
+      # save するまではidがnullなのでファイル名はrandomで設定
+      require 'securerandom'
+      @post.image_name = SecureRandom.hex
+      image = params[:image]
+      File.binwrite("public/user_images/#{@post.image_name}", image.read)
+    end
     @post.save
     redirect_to("/home/top")
   end
