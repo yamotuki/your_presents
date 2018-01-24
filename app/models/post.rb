20aab9089a0387be4db0697fde6a0db30b3d228f
Tomoya_Suzuki <mamemochi3ko@gmail.com>
2018-01-25 02:16:02 +0900

    paperclipのValidateに拡張子チェックを追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
-  include Paperclip::Glue
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  has_attached_file :image, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+
+  validates_attachment_content_type :image, content_type: /\Aimage/
+  validates_attachment_file_name :image, matches: [/png\z/, /PNG\z/, /jpe?g\z/, /JPE?G\z/]
 end
