4a7d5be56a79bcd96de4a18daa96c4aefa34676e
Tomoya_Suzuki <mamemochi3ko@gmail.com>
2018-01-27 22:42:30 +0900

    rails g model users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180127133640) do
+ActiveRecord::Schema.define(version: 20180127133941) do
 
   create_table "posts", force: :cascade do |t|
     t.string "situation"
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 20180127133640) do
     t.string "image_content_type"
     t.integer "image_file_size"
     t.datetime "image_updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "provider"
+    t.string "provider_uid"
+    t.string "nickname"
+    t.string "image_uri"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
