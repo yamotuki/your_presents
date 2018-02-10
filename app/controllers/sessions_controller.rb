cad67509b30380631d5371be1cb88f8943519303
Tomoya_Suzuki <mamemochi3ko@gmail.com>
2018-02-10 19:05:38 +0900

    ログイン成功時にflashでその旨が出るようにした

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   def create
     @user = User.find_or_create_from_auth(auth_hash)
+    flash[:notice] = "#{@user.nickname}でログインしました"
     redirect_to '/'
   end
 
